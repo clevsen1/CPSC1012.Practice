cada7289b65c1b68b523bf048f6f801cd4699fc6
clevsen1 <corylevsen@hotmail.com>
2021-01-27 10:43:27 -0800

    if else if examples

---
+++ if-else-if weather example/if-else-if weather example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace if_else_if_weather_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FahrenheitTemperature = 75;
+            int CelciusTemperature = 0;
+            FahrenheitTemperature = int.Parse(Console.ReadLine());
+
+            CelciusTemperature = (FahrenheitTemperature - 32) * 5 / 9;
+
+            if (CelciusTemperature < 0)
+            {
+                Console.WriteLine("It's freezing out!");
+            }
+            else if (CelciusTemperature < 15)
+            {
+                Console.WriteLine("Wear a jacket.");
+            }
+            else if (CelciusTemperature < 30)
+            {
+                Console.WriteLine("Its gorgeous outside.");
+            }
+            else
+            {
+                Console.WriteLine("It's finally summer!");
+            }
+        }
+    }
+}
---
+++ loan example/loan example/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace loan_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double loanAmount = 35000.00;
+            //int yearsWorked = 3;
+            //Console.Write("blah blah: ");
+
+            //if (loanAmount < 30000.00)
+            //{
+            //    if (yearsWorked < 2)
+            //    {
+            //        Console.WriteLine("You are not qualified for a loan due to low wages and in suffecient years worked");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You are not qualified for a loan due to in suffecient years worked");
+            //    }
+            //}
+            //else
+            //{
+            //    if (yearsWorked < 2)
+            //    {
+            //        Console.WriteLine("You are not qualified for a loan due to in suffecient years worked");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You are qualified for a loan");
+            //}
+            //}
+            
+            //if-else-if structure
+
+            int testScore = 75;
+
+            testScore = int.Parse(Console.ReadLine());
+
+            if (testScore < 50)
+            {
+                Console.WriteLine("Your grade is a F");
+            }
+            else if (testScore < 60)
+            {
+                Console.WriteLine("Your grade is a D");
+            }
+            else if (testScore < 70)
+            {
+                Console.WriteLine("Your grade is a C");
+            }
+            else if (testScore < 80)
+            {
+                Console.WriteLine("Your grade is a B");
+            }
+            else if (testScore < 90)
+            {
+                Console.WriteLine("Your grade is a A");
+            }
+        }
+    }
+}
